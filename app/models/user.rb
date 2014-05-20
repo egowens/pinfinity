e4697aa45e8b3c8dc4948fa6c675d9b9ca226907
egowens <eric.gowens@gmail.com>
2014-05-20 15:35:52 +0000

    Added usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_uniqueness_of :username
+
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
 end
